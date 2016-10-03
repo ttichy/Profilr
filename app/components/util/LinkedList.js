95abd46d52d48df3b0828196ad9dc7f489a7e850
Tom Tichy <ttichy@yahoo.com>
2016-10-02 21:44:59 -0400

    added linked list + spec

---
+++ app/components/util/LinkedList.js
@@ -0,0 +1,279 @@
+'use strict';
+
+define(['angular'],
+	function(angular) {
+		angular.module('profileEditor.ll',[])
+			.factory('LinkedList', [function() {
+
+
+				/**
+				 * Node of the linked list
+				 * @param {Ojbect} data data object
+				 */
+				var Node = function(data) {
+					this.data = data;
+					this.next = null;
+					this.previous = null;
+				};
+
+
+				/**
+				 * Double linked list functionality
+				 * some code swiped from: https://code.tutsplus.com/articles/data-structures-with-javascript-singly-linked-list-and-doubly-linked-list--cms-23392
+				 */
+				var LinkedList = function() {
+					this._length = 0;
+					this.head = null;
+					this.tail = null;
+				};
+				/**
+				 * Add a value at the end of the list
+				 * @param {Object} value value to add
+				 */
+				LinkedList.prototype.add = function(value) {
+					var node = new Node(value);
+
+					if (this._length) {
+						this.tail.next = node;
+						node.previous = this.tail;
+						this.tail = node;
+					} else {
+						this.head = node;
+						this.tail = node;
+					}
+
+					this._length++;
+
+					return node;
+				};
+
+
+				LinkedList.prototype.clearAll = function() {
+					this._length = 0;
+					this.head = null;
+					this.tail = null;
+				};
+
+
+				/**
+				 * Inserts into the list using an existing node
+				 * @param  {Node} existing existing Node
+				 * @param  {Object} data     new data to insert before existing node
+				 * @return {Node}          new node that was inserted
+				 */
+				LinkedList.prototype.insertAt = function(existing, data) {
+					var node = new Node(data);
+					var next = existing.next;
+					var prev = existing.previous;
+
+					node.next = existing;
+					existing.previous = node;
+
+					// if there is a previous node, wire it up
+					if (prev) {
+						prev.next = node;
+						node.previous = prev;
+					}
+
+					this._length++;
+					return node;
+
+				};
+
+
+				/**
+				 * Removes nodes specified by the parameter
+				 * @param  {Node} node Node to remove
+				 * @return {Node}      removed node
+				 */
+				LinkedList.prototype.removeNode = function(node) {
+					if (!(node instanceof Node))
+						throw new Error('removeNode: expecting a Node as parameter type');
+
+					var next = node.next;
+					var prev = node.previous;
+
+					//this could be the last node
+					if (next)
+						next.previous = prev;
+
+					//could be the first node
+					if (prev)
+						prev.next = next;
+					else
+						this.head = next;
+
+					var nodeToRemove = node;
+					node = null;
+
+					this._length--;
+
+					return nodeToRemove;
+
+				};
+
+
+				/**
+				 * Returns current length of the linked list
+				 */
+				LinkedList.prototype.length = function() {
+					return this._length;
+				};
+
+
+				/**
+				 * Gets all nodes currently in the list
+				 * @returns {Array} array of all nodes in the list
+				 */
+				LinkedList.prototype.getAllNodes = function() {
+					var result = [];
+
+					var currentNode = this.head;
+
+					while (currentNode) {
+						result.push(currentNode);
+						currentNode = currentNode.next;
+					}
+
+
+					return result;
+				};
+
+
+
+				/**
+				 * Gathers data from all nodes into an array
+				 * @returns {Array} array of all nodes in the list
+				 */
+				LinkedList.prototype.getDataArray = function() {
+					var result = [];
+
+					var currentNode = this.head;
+
+					while (currentNode) {
+						result.push(currentNode.data);
+						currentNode = currentNode.next;
+					}
+
+
+					return result;
+				};
+
+
+
+				/**
+				 * Get node at the specified position
+				 * @param  {Number} position position to get node at
+				 * @return {Node}          Node at specified position
+				 */
+				LinkedList.prototype.searchNodeAt = function(position) {
+					var currentNode = this.head,
+						length = this._length,
+						count = 1,
+						message = {
+							failure: 'Failure: non-existent node in this list.'
+						};
+
+					// 1st use-case: an invalid position
+					if (length === 0 || position < 1 || position > length) {
+						throw new Error(message.failure);
+					}
+
+					// 2nd use-case: a valid position
+					while (count < position) {
+						currentNode = currentNode.next;
+						count++;
+					}
+
+					return currentNode;
+				};
+
+
+
+				/**
+				 * Removes node at specified position
+				 * @param  {Number} position node at this position will be deleted
+				 * @return {Object}          Deleted node
+				 */
+				LinkedList.prototype.remove = function(position) {
+					var currentNode = this.head,
+						length = this._length,
+						count = 1,
+						message = {
+							failure: 'Failure: non-existent node in this list.'
+						},
+						beforeNodeToDelete = null,
+						nodeToDelete = null,
+						deletedNode = null;
+
+					// 1st use-case: an invalid position
+					if (length === 0 || position < 1 || position > length) {
+						throw new Error(message.failure);
+					}
+
+					// 2nd use-case: the first node is removed
+					if (position === 1) {
+						this.head = currentNode.next;
+						deletedNode = currentNode;
+
+						// 2nd use-case: there is a second node
+						if (!this.head) {
+							this.head.previous = null;
+							// 2nd use-case: there is no second node
+						} else {
+							this.tail = null;
+						}
+
+						this._length--;
+
+						return deletedNode;
+
+					}
+
+					// 3rd use-case: the last node is removed
+					if (position === this._length) {
+						deletedNode = this.tail;
+						this.tail = this.tail.previous;
+						this.tail.next = null;
+
+						this._length--;
+						return deletedNode;
+
+					}
+
+					// 4th use-case: a middle node is removed
+
+					while (count < position) {
+						currentNode = currentNode.next;
+						count++;
+					}
+
+					beforeNodeToDelete = currentNode.previous;
+					nodeToDelete = currentNode;
+					var afterNodeToDelete = currentNode.next;
+
+					beforeNodeToDelete.next = afterNodeToDelete;
+					afterNodeToDelete.previous = beforeNodeToDelete;
+					deletedNode = nodeToDelete;
+					nodeToDelete = null;
+
+
+					this._length--;
+
+					return deletedNode;
+				};
+
+
+				var factory = {};
+
+				/**
+				 * Creates a new linked list
+				 */
+				factory.makeLinkedList = function() {
+					return new LinkedList();
+				};
+
+
+				return factory;
+			}]);
+	});
---
+++ app/components/util/linkedList.spec.js
@@ -0,0 +1,249 @@
+/* global describe, it, expect, beforeEach, afterEach, module, inject */
+'use strict';
+
+define(['app', 'angularMocks','components/util/LinkedList'], function(app) {
+  describe('linked list', function() {
+    var linkedList;
+
+    beforeEach(module('profileEditor.ll'));
+
+
+    beforeEach( inject( function(_LinkedList_){
+      linkedList = _LinkedList_;
+    }));
+
+
+
+it("Should add 2 nodes successfully", function() {
+
+    var list = linkedList.makeLinkedList();
+    expect(list).not.toBe(null);
+
+    var data1 = {
+      "data1": 1
+    };
+    var data2 = {
+      "data2": 2
+    };
+
+    var data1p5 = {
+      "data1.5": 1.5
+    };
+
+    list.add(data1);
+    expect(list.length()).toBe(1);
+    expect(list.searchNodeAt(1).data).toBe(data1);
+
+    list.add(data2);
+    expect(list.length()).toBe(2);
+    expect(list.searchNodeAt(2).data).toBe(data2);
+
+
+  });
+
+
+  it("Should add 2 nodes and insert 3rd in the middle", function() {
+
+    var list = linkedList.makeLinkedList();
+    expect(list).not.toBe(null);
+
+    var data1 = {
+      "data1": 1
+    };
+    var data2 = {
+      "data2": 2
+    };
+
+    var data1p5 = {
+      "data1.5": 1.5
+    };
+
+    list.add(data1);
+    var node=list.add(data2);
+
+    list.insertAt(node,data1p5);
+
+    expect(list.length()).toBe(3);
+    expect(list.searchNodeAt(2).data).toBe(data1p5);
+    expect(list.searchNodeAt(1).data).toBe(data1);
+    expect(list.searchNodeAt(3).data).toBe(data2);
+
+  });
+
+
+
+  it("Should add 2 nodes and insert 3rd in the middle, then delete the middle using the remove function", function() {
+
+    var list = linkedList.makeLinkedList();
+    expect(list).not.toBe(null);
+
+    var data1 = {
+      "data1": 1
+    };
+    var data2 = {
+      "data2": 2
+    };
+
+    var data1p5 = {
+      "data1.5": 1.5
+    };
+
+    list.add(data1);
+    var node=list.add(data2);
+
+    var middle=list.insertAt(node,data1p5);
+
+    expect(list.length()).toBe(3);
+    expect(list.searchNodeAt(2).data).toBe(data1p5);
+    expect(list.searchNodeAt(1).data).toBe(data1);
+    expect(list.searchNodeAt(3).data).toBe(data2);
+
+
+    var removed=list.remove(2);
+    expect(removed).toBe(middle);
+
+
+  });
+
+
+
+  it("Should add 2 nodes and insert 3rd in the middle, then delete the middle using the removeNode function", function() {
+
+    var list = linkedList.makeLinkedList();
+    expect(list).not.toBe(null);
+
+    var data1 = {
+      "data1": 1
+    };
+    var data2 = {
+      "data2": 2
+    };
+
+    var data1p5 = {
+      "data1.5": 1.5
+    };
+
+    list.add(data1);
+    var node=list.add(data2);
+
+    var middle=list.insertAt(node,data1p5);
+
+    expect(list.length()).toBe(3);
+    expect(list.searchNodeAt(2).data).toBe(data1p5);
+    expect(list.searchNodeAt(1).data).toBe(data1);
+    expect(list.searchNodeAt(3).data).toBe(data2);
+
+
+    var removed=list.removeNode(middle);
+    expect(removed).toBe(middle);
+
+    expect(list.length()).toBe(2);
+    expect(list.searchNodeAt(1).data).toBe(data1);
+    expect(list.searchNodeAt(2).data).toBe(data2);
+
+  });
+
+
+  it("Should get data of length 3 from list of length 3", function(){
+    var list = linkedList.makeLinkedList();
+
+    var data1 = {
+      "data1": 1
+    };
+    var data2 = {
+      "data2": 2
+    };
+
+    var data3 = {
+      "data3": 3
+    };    
+
+
+    list.add(data1);
+    list.add(data2);
+    list.add(data3);
+
+    var datas=list.getDataArray();
+
+    expect(datas[0]).toBe(data1);
+    expect(datas[1]).toBe(data2);
+    expect(datas[2]).toBe(data3);
+
+    expect(datas.length).toBe(3);
+
+
+  });
+
+
+  it("getDataArray method should get empty array from empty list",function(){
+
+    var list=linkedList.makeLinkedList();
+
+    var datas=list.getDataArray();
+
+    expect(datas.length).toBe(0);
+
+  });
+
+
+  it("should successfully delete the first node", function() {
+
+    var list = linkedList.makeLinkedList();
+
+    var data1 = {
+      "data1": 1
+    };
+    var data2 = {
+      "data2": 2
+    };
+
+    var data3 = {
+      "data3": 3
+    };    
+
+
+    var first = list.add(data1);
+    list.add(data2);
+    list.add(data3);
+
+    var removed=list.removeNode(first);
+
+    expect(removed).toBe(first);
+
+
+  });
+
+  it("should successfully delete the last node", function() {
+
+    var list = linkedList.makeLinkedList();
+
+    var data1 = {
+      "data1": 1
+    };
+    var data2 = {
+      "data2": 2
+    };
+
+    var data3 = {
+      "data3": 3
+    };    
+
+
+    list.add(data1);
+    list.add(data2);
+    var last = list.add(data3);
+
+    var removed=list.removeNode(last);
+
+    expect(removed).toBe(last);
+
+
+  });
+
+
+  });
+});
+
+
+
+
